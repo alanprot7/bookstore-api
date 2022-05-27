edb5ade7c3c3fd3ffc12edfc0dbaf21787ce4138
Alan Oliveira <alanprot7@gmail.com>
2022-05-27 19:56:16 -0300

    perfil de teste implementado

--- src/main/java/com/alan/bookstore/BookstoreApplication.java
+++ src/main/java/com/alan/bookstore/BookstoreApplication.java
@@ -1,38 +1,13 @@
 package com.alan.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.alan.bookstore.domain.Categoria;
-import com.alan.bookstore.domain.Livro;
-import com.alan.bookstore.repositories.CategoriaRepository;
-import com.alan.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
-		Livro l1 = new Livro(null, "Clean Code", "Martin Robert", "Lorem ipsum", cat1);
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
 	}
 
 }
